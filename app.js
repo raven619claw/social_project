8a3e40fd8693e7aed333cf59c8b27d872aa8c3e7
ravi <ravi.yadav@proptiger.com>
2017-02-17 18:44:32 +0530

    removed browser refresh / used nodemon / load template while rendering

--- app.js
+++ app.js
@@ -5,8 +5,4 @@ require('./src/config/server')(app);
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
-
-    if (process.send) {
-        process.send('online');
-    }
 });
--- src/services/controllers.js
+++ src/services/controllers.js
@@ -4,7 +4,6 @@ var rootPath = path.join(__dirname, '../');
 module.exports = function() {
     var controllersPath = {};
     var controllers = glob.sync(rootPath + '/modules/**/controller.js');
-    console.log(controllers);
     for (var i = 0; i < controllers.length; i++) {
     	var substr = controllers[i].slice(0,controllers[i].lastIndexOf("/"));
         controllersPath[(substr.slice(substr.lastIndexOf("/") + 1))] = require(controllers[i]);
