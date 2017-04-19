3b030e50d6a6a85f13c47aee14747c7f0bde26b3
ravi <ravi.yadav@proptiger.com>
2017-04-19 22:09:48 +0530

    changes

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -5,8 +5,9 @@ const GLOBALCONSTANTS = require('../config/constants');
 
 let host = GLOBALCONSTANTS.APPCONFIG.HOST+'/apis';
 let apiService = {};
-
+console.log(host);
 apiService.get = (url) => {
+	console.log(host + url);
     return axios.get(host + url);
 };
 
