141b714eb673b146b55eb973c7fd3d4cbe95208b
ravi <ravi.yadav@proptiger.com>
2017-04-19 21:56:39 +0530

    changes

--- src/modules/home/controller.js
+++ src/modules/home/controller.js
@@ -11,7 +11,9 @@ let loader = function(req, res) {
         pageData = {};
         let userData = sessionGlobal.getUserDataFromSession(req.session);
         pageData.userData = userData;
+        console.log(userData);
         if (userData.success) {
+            console.log(userData);
             getPostData(userData.user.userId).then((result) => {
                 pageData.userPostData = result.data;
                 resolve(pageData);
@@ -20,6 +22,7 @@ let loader = function(req, res) {
             });
         } else {
             resolve(pageData);
+            console.log(pageData);
         }
     });
 
--- src/services/templateLoader.js
+++ src/services/templateLoader.js
@@ -6,6 +6,7 @@ let templateLoader = (template) => {
     let layoutData = { title: 'social_project' };
     template.renderTemplate = (dataFn, req, res) => {
         dataFn(req, res).then((result) => {
+        	console.log(result)
             layoutData.data = result;
             template.render(layoutData, res);
         });
