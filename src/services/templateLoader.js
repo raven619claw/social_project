d443eb2886956e3da9358fd18b92eb2c719536f6
ravi <ravi.yadav@proptiger.com>
2017-05-16 21:15:13 +0530

    added nod engine for heroku

--- src/services/templateLoader.js
+++ src/services/templateLoader.js
@@ -4,7 +4,7 @@ const marko = require('marko');
 let templateLoader = (template) => {
     template = marko.load(template);
     let layoutData = { title: 'social_project' };
-    template.renderTemplate = async(dataFn, req, res) => {
+    template.renderTemplate = async (dataFn, req, res) => {
         try {
             // this parse may fail
             let result = await dataFn(req, res);
